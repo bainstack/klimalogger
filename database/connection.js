1f0fe344e684646aeb2420f4736b937a35983392
bainstack <sebastianlieschke@web.de>
2018-01-18 15:29:32 +0100

    changed to sqlite database; added function for creating dummy data; added function for altering data; added function to insert data

--- database/connection.js
+++ database/connection.js
@@ -1,61 +1,71 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'basti',
-    password: 'Knaller20',
-    database: 'metzstrassenklima'
+const sqlite3 = require('sqlite3').verbose();
+
+//connect to database
+let db = new sqlite3.Database('./../db_klimalogger/db_klimalogger.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    else console.log('Connected to the database: klimalogger!');
 });
 
-connection.connect(function (err) {
-    if (!err) {
-        console.log("Database successfully connected!\n");
-    } else {
-        console.log("Error connecting database!\n");
-    }
+db.serialize(() => {
+
+    //insertRandomData(100, "temperature_humidity", "temperature, humidity, date_time");
+
+    let sql = (`SELECT id as id,
+    temperature as temperature,
+        humidity as humidity,
+        date_time as date_time
+    FROM temperature_humidity`)
+    db.each(sql, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log(row.id + "\t" + row.date_time + "\t" + row.temperature + "\t" + row.humidity);
+    });
 });
 
-function insertData(table_name) {
-    let values = new Array();
-    var sql = "INSERT INTO klima (temperature, humidity, reg_time, reg_date) VALUES ?";
-    var i = 0;
-    do {
-        console.log('Array an der Stelle ' + i);
-        let randomTemperature = (Math.random() * 61 - 20);
-        let randomHumidity = (Math.random() * 101);
-        let randomreg_time =
-            ((Math.floor(Math.random() * (Math.floor(24) - Math.ceil(0))) + Math.ceil(0)) + ":" +
-                (Math.floor(Math.random() * (Math.floor(61) - Math.ceil(0))) + Math.ceil(0)) + ":" +
-                (Math.floor(Math.random() * (Math.floor(61) - Math.ceil(0))) + Math.ceil(0)));
-        console.log(randomreg_time);
-        let randomreg_date =
-            ((Math.floor(Math.random() * (Math.floor(32) - Math.ceil(1))) + Math.ceil(1)) + "." +
-                (Math.floor(Math.random() * (Math.floor(13) - Math.ceil(1))) + Math.ceil(1)) + "." +
-                (Math.floor(Math.random() * (Math.floor(2018) - Math.ceil(1950))) + Math.ceil(1950)));
-        console.log(randomreg_date);
-        //console.log(values.length);
-        //console.log(randomTemperature);
-        values[i] = [randomTemperature, randomHumidity, randomreg_time, randomreg_date];
-        i++;
+db.close((err) => {
+    if (err) {
+        console.error(err.message);
     }
-    while (i < 100);
-    /*
-    [
-    
-    ]
-    connection.query(sql, [values], function (err) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
-    })
-    */
+    console.log('Closed the database connection.');
+});
+
+function createRandomdata() {
+    let randomTemperature = (Math.random() * 61 - 20);
+    let randomHumidity = (Math.random() * 101);
+    let randomHour = checkrandomsforzero(Math.floor(Math.random() * (Math.floor(24) - Math.ceil(0))) + Math.ceil(0));
+    let randomMinute = checkrandomsforzero(Math.floor(Math.random() * (Math.floor(61) - Math.ceil(0))) + Math.ceil(0));
+    let randomSecond = checkrandomsforzero(Math.floor(Math.random() * (Math.floor(61) - Math.ceil(0))) + Math.ceil(0));
+    let randomDay = checkrandomsforzero((Math.floor(Math.random() * (Math.floor(32) - Math.ceil(1))) + Math.ceil(1)));
+    let randomMonth = checkrandomsforzero((Math.floor(Math.random() * (Math.floor(13) - Math.ceil(1))) + Math.ceil(1)));
+    let randomYear = checkrandomsforzero((Math.floor(Math.random() * (Math.floor(2018) - Math.ceil(1950))) + Math.ceil(1950)));
+    let randomreg_datetime = randomYear + '-' + randomMonth + '-' + randomDay + ' ' + randomHour + ':' + randomMinute + ':' + randomSecond;
+    let data = [randomTemperature, randomHumidity, randomreg_datetime];
+    console.log(data);
+    return data;
 };
 
-insertData();
+function checkrandomsforzero(random) {
+    if (random < 10) {
+        random = '0' + random;
+    }
+    return random;
+}
 
-let table_klima = connection.query('SELECT * from klima', function (err, rows, fields) {
-    if (!err)
-        console.log('Successfully loaded table KLIMA with: ', rows);
-    else
-        console.log('Error while performing Query for table KLIMA.', err);
-});
-
-module.exports = table_klima;
+function insertRandomData(amount, table, columns) {
+    for (j = 0; j < 100; j++) {
+        let randomData = createRandomdata();
+        let placeholders = "(" + randomData.map((randomData) => '?').join(',') + ")";
+        let sql = "INSERT INTO " + table + "(" + columns + ")" + " VALUES " + placeholders;
+        console.log(sql);
+        console.log(randomData);
+        db.run(sql, randomData, function (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            //console.log(`Rows inserted ${this.changes}`);
+        });
+    };
+};
